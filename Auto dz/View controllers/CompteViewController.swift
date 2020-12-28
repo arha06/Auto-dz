ddf89e77c5be2140e1d69a915e99a1143b11d96d
hadj aissa hadj said <hadjaissahadjsaid@hadjs-MacBook-Pro.local>
2020-12-28 19:22:49 +0300

    made with a tabBarController and a login page

---
+++ Auto dz/View controllers/CompteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CompteViewController.swift
+//  Auto dz
+//
+//  Created by hadj aissa hadj said on 26.12.2020.
+//
+
+import UIKit
+
+class CompteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
